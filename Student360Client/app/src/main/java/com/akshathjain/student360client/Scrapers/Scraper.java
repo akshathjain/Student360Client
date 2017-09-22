5d7c645005723fbc1a141a47bdbf492ca101bc9b
Akshath Jain <akshath.r.jain@gmail.com>
2017-09-21 21:25:59 -0400

    stuff and things

---
+++ Student360Client/app/src/main/java/com/akshathjain/student360client/Scrapers/Scraper.java
@@ -0,0 +1,10 @@
+package com.akshathjain.student360client.Scrapers;
+
+/*
+Name: Akshath Jain
+Date: 9/21/17
+Purpose: abstract Scraper class for webscraping
+ */
+
+public abstract class Scraper {
+}
